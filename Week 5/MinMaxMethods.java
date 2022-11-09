8133c39f85335110400d6518e114c118b8b89b23
Jack Briggs <83983404+jack-briggsUK@users.noreply.github.com>
2022-11-09 21:51:19 +0000

    Adding comments and closing scan

--- Week 5/MinMaxMethods.java
+++ Week 5/MinMaxMethods.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class MinMaxMethods {
+    //returns the minimum Int
     public static int minimum(int x, int y, int z) {
         if (x < y && x < z) {
             return x;
@@ -10,7 +11,7 @@ public class MinMaxMethods {
             return z;
         }
     }
-
+//returns the maximum Int
     public static int maximum(int x, int y, int z) {
         if (x > y && x > z) {
             return x;
@@ -20,7 +21,7 @@ public class MinMaxMethods {
             return z;
         }
     }
-
+//return the minimum Double
     public static double minimum(double x, double y, double z) {
         if (x < y && x < z) {
             return x;
@@ -30,7 +31,7 @@ public class MinMaxMethods {
             return z;
         }
     }
-
+//return the maximum Double
     public static double maximum(double x, double y, double z) {
         if (x > y && x > z) {
             return x;
@@ -40,7 +41,7 @@ public class MinMaxMethods {
             return z;
         }
     }
-
+//prints the min and max Int
     public static void intMethod(int num1) {
         Scanner scan = new Scanner(System.in);
         System.out.println("And the second:");
@@ -50,8 +51,9 @@ public class MinMaxMethods {
 
         System.out.println("The min value is: " + minimum(num1, num2, num3));
         System.out.println("The max value is: " + maximum(num1, num2, num3));
+        scan.close();
     }
-
+//prints the min and max Double
     public static void doubleMethod(Double num1) {
         Scanner scan = new Scanner(System.in);
         System.out.println("And the second:");
@@ -61,10 +63,11 @@ public class MinMaxMethods {
 
         System.out.println("The min value is: " + minimum(num1, num2, num3));
         System.out.println("The max value is: " + maximum(num1, num2, num3));
+        scan.close();
     }
 
+    //MAIN determins if Int or Double
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter first number:");
         if (scan.hasNextInt()) {
@@ -72,5 +75,6 @@ public class MinMaxMethods {
         } else if (scan.hasNextDouble()) {
             doubleMethod(scan.nextDouble());
         }
+        scan.close();
     }
 }
