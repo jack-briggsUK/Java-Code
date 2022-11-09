3a37fd1780f96e56a872b4025564c8fa79562598
Jack Briggs <83983404+jack-briggsUK@users.noreply.github.com>
2022-11-09 21:46:01 +0000

    Fixing Fib and MinMax

--- Week 5/FibonacciCalc.java
+++ Week 5/FibonacciCalc.java
@@ -1,22 +1,41 @@
 public class FibonacciCalc {
-    static void Fibonacci(int x) {
-        double[] num = new double[x];
+//     static void Fibonacci(int x) {
+//         double[] num = new double[x];
 
-        num[0] = 0;
-        num[1] = 1;
-        System.out.println(num[0]);
-        System.out.println(num[1]);
+//         num[0] = 0;
+//         num[1] = 1;
+//         System.out.println(num[0]);
+//         System.out.println(num[1]);
 
-        for (int i = 2; i < num.length; i++) {
-            int j = i - 2;
-            int k = i - 1;
-            num[i] = num[j] + num[k];
+//         for (int i = 2; i < num.length; i++) {
+//             int j = i - 2;
+//             int k = i - 1;
+//             num[i] = num[j] + num[k];
 
-            System.out.println(num[i]);
-        }
+//             System.out.println(num[i]);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Fibonacci(100);
+//     }
+public static long Fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return Fibonacci(n - 2) + Fibonacci(n - 1);
+}
+
+public static void main(String[] args) {
+    int max = 100;
+    System.out.print("Fibonacci Series of " + max + " numbers: ");
+    for (int i = 0; i < max; i++) {
+        System.out.print(Fibonacci(i) + " ");
     }
 
-    public static void main(String[] args) {
-        Fibonacci(100);
-    }
+}
+
 }
--- Week 5/MinMaxMethods.java
+++ Week 5/MinMaxMethods.java
@@ -41,18 +41,36 @@ public class MinMaxMethods {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int num1 = Integer.parseInt(reader.nextLine());
+    public static void intMethod(int num1) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("And the second:");
-        int num2 = Integer.parseInt(reader.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
         System.out.println("And the final number:");
-        int num3 = Integer.parseInt(reader.nextLine());
+        int num3 = Integer.parseInt(scan.nextLine());
 
         System.out.println("The min value is: " + minimum(num1, num2, num3));
         System.out.println("The max value is: " + maximum(num1, num2, num3));
+    }
 
-        reader.close();
+    public static void doubleMethod(Double num1) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("And the second:");
+        double num2 = Double.parseDouble(scan.nextLine());
+        System.out.println("And the final number:");
+        double num3 = Double.parseDouble(scan.nextLine());
+
+        System.out.println("The min value is: " + minimum(num1, num2, num3));
+        System.out.println("The max value is: " + maximum(num1, num2, num3));
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        if (scan.hasNextInt()) {
+            intMethod(scan.nextInt());
+        } else if (scan.hasNextDouble()) {
+            doubleMethod(scan.nextDouble());
+        }
     }
 }
